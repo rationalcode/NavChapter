00799eb1bde8e4d8dae38836b773371fe650ced0
rationalcode <marat_pochta@mail.ru>
2018-07-02 17:49:51 +0500

    Initial commit

---
+++ app/src/main/java/com/example/admin/navchapter/ChapterFrame.java
@@ -0,0 +1,22 @@
+package com.example.admin.navchapter;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+public class ChapterFrame extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        //return super.onCreateView(inflater, container, savedInstanceState);
+
+        return inflater.inflate(R.layout.chapter_menu_fragment, container, false);
+    }
+}
