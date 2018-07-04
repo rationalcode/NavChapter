44a4cd1373ec06af2f0f6372cb4e1f7ab329d872
rationalcode <marat_pochta@mail.ru>
2018-07-04 10:45:18 +0500

    default fragment

---
+++ app/src/main/java/com/example/admin/navchapter/DefaultFragment.java
@@ -0,0 +1,17 @@
+package com.example.admin.navchapter;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DefaultFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.default_fragment_chapter, container, false);
+    }
+}
